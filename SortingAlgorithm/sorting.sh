691f80ce0291338a6c64417f6bc07c64515fd592
Bea <beatriz.rodrigues@eqsglobal.com>
2022-11-03 14:49:29 +0000

    OB-13 final

--- SortingAlgorithm/sorting.sh
+++ SortingAlgorithm/sorting.sh
@@ -1,6 +1,38 @@
-n=0
-while read line
+#!/bin/bash
+readArray(){
+	COUNTER=0
+	while read line
+		do
+			if [[ $line != "" ]];
+				then 
+					my_array=("${my_array[@]}" $line)
+					let COUNTER++
+				else 
+				break
+			fi
+		done
+}
+
+sort(){
+	for((i=0; i<COUNTER-1; i++))
 	do
-		my_array=("${my_array[n]}" $line)
+		for((j=0; j<COUNTER-1;j++))
+		do
+			if [ ${my_array[j]} -gt ${my_array[$((j+1))]} ]
+			then
+
+				TEMPORARY=${my_array[j]}
+				my_array[$j]=${my_array[$((j+1))]}
+				my_array[$((j+1))]=$TEMPORARY
+			fi
+		done
 	done
-echo "${my_array}"
+
+}
+
+
+readArray
+sort
+
+echo "${my_array[@]}"
+echo "The array size is ${COUNTER}"
